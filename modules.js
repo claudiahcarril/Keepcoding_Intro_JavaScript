0dd3630158d904f3ad81e2df5eaf1c124cafc288
Claudia Hernández <claudiacarril8@gmail.com>
2022-11-25 16:38:22 +0100

    module.js (showRequeriments)

--- modules.js
+++ modules.js
@@ -14,7 +14,15 @@ export const students = [{
 export const availableMaleNames = ['pepe', 'juan', 'victor', 'Leo', 'francisco', 'carlos'];
 export const availableFemaleNames = ['cecilia', 'ana', 'luisa', 'silvia', 'isabel', 'virginia'];
 export const availableGenders = ['male', 'female'];
-  
+
+
+
+// REQUISITOS
+export function showRequirements(){
+    console.log("\n1: Mostrar en formato de tabla todos los alumnos\n2: Mostrar por consola la cantidad de alumnos que hay en clase\n3: Mostrar por consola todos los nombres de los alumnos\n4: Eliminar el último alumno de la clase\n5: Eliminar un alumno aleatoriamente de la clase\n6: Mostrar por consola todos los datos de los alumnos que son chicas\n7: Mostrar por consola el número de chicos y chicas que hay en la clase\n8: Mostrar true o false por consola si todos los alumnos de la clase son chicas\n9: Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años\n10: Añadir un alumno nuevo con los siguientes datos: nombre aleatorio, edad aleatoria entre 20 y 50 años, género aleatorio, listado de calificaciones vacío\n11: Mostrar por consola el nombre de la persona más joven de la clase\n12: Mostrar por consola la edad media de todos los alumnos de la clase\n13: Mostrar por consola la edad media de las chicas de la clase\n14: Añadir nueva nota a los alumnos. Por cada alumno de la clase, tendremos que calcular una nota de forma aleatoria(número entre 0 y 10) y añadirla a su listado de notas\n15: Ordenar el array de alumnos alfabéticamente según su nombre\n16: Mostrar por consola el alumno de la clase con las mejores notas\n17: Mostrar por consola la nota media más alta de la clase y el nombre del alumno al que pertenece\n18: Añadir un punto extra a cada nota existente de todos los alumnos\n")
+}
+
+
   
 //   1- Mostrar en formato de tabla todos los alumnos.
 export function showStudents(students) {
